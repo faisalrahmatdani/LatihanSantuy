0aab6bff0799fa707a47f9d00bbd6b4482e3b1c2
alifah cahyati <faisalrahmatdani07@gmail.com>
2022-03-10 22:13:38 +0700

    sidebar-responsive

--- src/containers/MainPage.jsx
+++ src/containers/MainPage.jsx
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import { Route, Routes } from "react-router-dom";
+import { BiMenuAltLeft } from 'react-icons/bi';
+import { AiOutlineClose } from 'react-icons/ai';
 import Sidebar from "../components/Sidebar";
 import About from "../pages/About";
 import Blog from "../pages/Blog";
@@ -7,13 +9,29 @@ import Content from "../pages/Content";
 import Home from "../pages/Home";
 
 const MainPage = () => {
+
+  const [toggleSideBar, setToggleSidebar] = useState(false)
   return (
     <div className="flex flex-col md:flex-row">
       <aside className="flex flex-col">
         <div className="hidden md:flex h-screen">
           <Sidebar />
         </div>
-        <div></div>
+        {!toggleSideBar && (
+          <div className="flex md:hidden hover:cursor-pointer px-3 py-3">
+          <BiMenuAltLeft fontSize={35} className='cursor-pointer' onClick={() => setToggleSidebar(true)}/>
+        </div>
+        )}
+        {toggleSideBar && (
+          <div className="w-64">
+            <div className="absolute w-64 flex justify-end px-3 py-3">
+              <AiOutlineClose fontSize={25} className='cursor-pointer text-white' onClick={() => setToggleSidebar(false)}/>  
+            </div>
+            <div className="h-screen">  
+            <Sidebar/>
+            </div>
+          </div>
+        )}
       </aside>
       <main className="w-full">
         <Routes>
